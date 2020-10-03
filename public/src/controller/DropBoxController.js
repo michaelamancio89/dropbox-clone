579bd240b841bb8d8b9996cb360ee3965d882fe4
Michael Amâncio <michael89amancio@gmail.com>
2020-10-03 15:22:12 -0300

    create formidable with files

--- public/src/controller/DropBoxController.js
+++ public/src/controller/DropBoxController.js
@@ -8,6 +8,12 @@ class DropBoxController{
 
         this.snackModalEl = document.querySelector('#react-snackbar-root');
 
+        this.progressBarEl = this.snackModalEl.querySelector('.mc-progress-bar-fg');
+
+        this.namefileEl = this.snackModalEl.querySelector('.namefile');
+
+        this.timeleftEl = this.snackModalEl.querySelector('.timeleft');
+
         this.initEvents();
     }
 
@@ -21,7 +27,7 @@ class DropBoxController{
 
         this.inputFilesEl.addEventListener('change', event =>{
 
-            console.log(event.target.files);
+            //console.log(event.target.files);
             this.uploadTask(event.target.files);
 
             this.snackModalEl.style.display = 'block';
@@ -55,9 +61,16 @@ class DropBoxController{
                     reject(event);
                 };
 
+                ajax.onprogress = event =>{
+
+                    this.uploadProgress(event, file);
+                }
+
                 let formData = new FormData();
 
                 formData.append('input-file',files);
+
+                this.startUploadTime = Date.now();
 
                 ajax.send(formData);
 
@@ -66,5 +79,58 @@ class DropBoxController{
         });
 
         return Promise.all(promises);
+    }
+
+    uploadProgress(event, file){
+
+        let timespent = Date.now() - this.startUploadTime;
+        let loaded = event.loaded;
+        let total = event.total;
+
+        let porcent = parseInt(loaded/total) * 100;
+
+        let timeleft = ((100 - porcent) * timespent)/ porcent;
+
+        this.progressBarEl.style.width = `${porcent}%`;
+
+        this.namefileEl.innerHTML = file.name;
+
+        this.timeleftEl.innerHTML = this.formatTimeToHuman(this.timeleft);
+
+        console.log(timespent, timeleft,porcent);
+
+
+    }
+
+    formatTimeToHuman(duration){
+
+        let seconds = parseInt((duration / 1000) % 60);
+
+        let minutes = parseInt((duration /(1000 * 60)) %60);
+
+        let hours = parseInt((duration /(1000 * 60 *60)) %24);
+
+        if(hours > 0){
+
+            return `${hours} horas,  ${minutes} minutos e ${seconds} segundos`;
+            
+        }
+
+        if(minutes > 0){
+
+            return `${minutes} minutos e ${seconds} segundos`;
+            
+        }
+
+        if(seconds > 0){
+
+            return `${seconds} segundos`;
+            
+        }
+
+
+
+
+
     }
 }
